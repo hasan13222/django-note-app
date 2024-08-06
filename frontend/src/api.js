dc0a3f7a689541d0701d28810b72cd7363d271ae
hasan13222 <jamil8305@gmail.com>
2024-08-06 23:57:52 +0600

    backend api url added

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constant";
-
+const apiUrl = "https://13cc055e-b3fe-49fc-8564-7bd134165757-dev.e1-us-cdp-2.choreoapis.dev/djangonoteapp/backend/v1";
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
 
 api.interceptors.request.use(
